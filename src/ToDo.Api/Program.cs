1af6bbd806da3f3d3afb64a37c539699d6c4f632
Cindy Conway <cconway@upwardsports.org>
2026-06-26 16:33:54 -0400

    add feature and spec and implementation files

--- src/ToDo.Api/Program.cs
+++ src/ToDo.Api/Program.cs
@@ -1,4 +1,5 @@
 using Csla.Configuration;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
